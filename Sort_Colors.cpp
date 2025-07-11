1654ced8247a43d1287b76c1bfb47f40c6c4537b
Akash-N1406 <ak54ash5@gmail.com>
2025-07-11 18:07:13 +0530

    Optimized

--- Sort_Colors.cpp
+++ Sort_Colors.cpp
@@ -9,31 +9,67 @@ You must solve this problem without using the library's sort function.*/
 #include <vector>
 using namespace std;
 
-class Solution {
+/*75. SORT COLORS
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
 public:
-    void sortColors(vector<int>& nums) {
-        vector<int> count(3, 0);
-        for (int num : nums) {
-            if (num == 0) count[0]++;
-            else if (num == 1) count[1]++;
-            else count[2]++;
-        }
-        for (int i = 0; i < nums.size(); i++) {
-            if (count[0] != 0) {
-                nums[i] = 0;
-                count[0]--;
+    void sortColors(vector<int> &nums)
+    {
+        int low = 0;
+        int mid = 0;
+        int high = nums.size() - 1;
+        while (mid <= high)
+        {
+            if (nums[mid] == 0)
+            {
+                swap(nums[mid], nums[low]);
+                low++;
+                mid++;
             }
-            else if (count[1] != 0) {
-                nums[i] = 1;
-                count[1]--;
-            }
-            else {
-                nums[i] = 2;
-                count[2]--;
+            else if (nums[mid] == 1)
+                mid++;
+            else
+            {
+                swap(nums[mid], nums[high]);
+                high--;
             }
         }
     }
 };
+
+int main()
+{
+    Solution sol;
+    vector<int> nums = {2, 0, 2, 1, 1, 0};
+
+    cout << "Before sorting: ";
+    for (int num : nums)
+    {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    sol.sortColors(nums);
+
+    cout << "After sorting: ";
+    for (int num : nums)
+    {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
 
 int main()
 {
