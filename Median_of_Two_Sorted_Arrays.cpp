b745a14e6f47666d65b2c09930e6856831ea3a38
Akash-N1406 <ak54ash5@gmail.com>
2025-07-23 19:56:16 +0530

    Update Median_of_Two_Sorted_Arrays.cpp
    Optimal Solution

--- Median_of_Two_Sorted_Arrays.cpp
+++ Median_of_Two_Sorted_Arrays.cpp
@@ -5,54 +5,69 @@ The overall run time complexity should be O(log (m+n)).*/
 
 #include <iostream>
 #include <vector>
+#include <climits>   // For INT_MIN, INT_MAX
+#include <stdexcept> // For invalid_argument
+
 using namespace std;
 
 class Solution
 {
-private:
-    vector<int> MergeandSort(vector<int> &nums1, vector<int> &nums2)
-    {
-        int i = 0, j = 0;
-        vector<int> result;
-
-        while (i < nums1.size() && j < nums2.size())
-        {
-            if (nums1[i] < nums2[j])
-            {
-                result.push_back(nums1[i++]);
-            }
-            else
-            {
-                result.push_back(nums2[j++]);
-            }
-        }
-        while (i < nums1.size())
-        {
-            result.push_back(nums1[i++]);
-        }
-        while (j < nums2.size())
-        {
-            result.push_back(nums2[j++]);
-        }
-        return result;
-    }
-
 public:
     double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
     {
-        vector<int> result = MergeandSort(nums1, nums2);
-        int len = result.size();
-        if (len % 2 != 0)
+        if (nums1.size() > nums2.size())
+            return findMedianSortedArrays(nums2, nums1);
+
+        int m = nums1.size();
+        int n = nums2.size();
+        int totalLeft = (m + n + 1) / 2;
+
+        int low = 0, high = m;
+
+        while (low <= high)
         {
-            return result[len / 2];
+            int i = low + (high - low) / 2;
+            int j = totalLeft - i;
+
+            int left1 = (i == 0) ? INT_MIN : nums1[i - 1];
+            int right1 = (i == m) ? INT_MAX : nums1[i];
+            int left2 = (j == 0) ? INT_MIN : nums2[j - 1];
+            int right2 = (j == n) ? INT_MAX : nums2[j];
+
+            if (left1 <= right2 && left2 <= right1)
+            {
+                if ((m + n) % 2 == 0)
+                {
+                    return (max(left1, left2) + min(right1, right2)) / 2.0;
+                }
+                else
+                {
+                    return max(left1, left2);
+                }
+            }
+            else if (left1 > right2)
+            {
+                high = i - 1;
+            }
+            else
+            {
+                low = i + 1;
+            }
         }
-        return (result[len / 2] + result[(len / 2) - 1]) / 2.0;
+
+        throw invalid_argument("Input arrays are not valid.");
     }
 };
 
-int main(){
+int main()
+{
     Solution sol;
-    vector<int> num1 = {1, 3};
-    vector<int> num2 = {2};
-    cout << "Median: " << sol.findMedianSortedArrays(num1, num2);
+
+    vector<int> nums1 = {1, 3};
+    vector<int> nums2 = {2};
+
+    double result = sol.findMedianSortedArrays(nums1, nums2);
+    cout << "Median is: " << result << endl;
+
+    return 0;
 }
